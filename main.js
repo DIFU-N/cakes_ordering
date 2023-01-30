ba5e336818472b75956ca4aa9258d793bab52558
DIFU-N <chukwudifunsofor@gmail.com>
2023-01-30 15:10:20 +0100

    changed state by ordering a cake

---
+++ main.js
@@ -0,0 +1,86 @@
+const redux = require('redux');
+const createStore = redux.createStore;
+
+const cakeOrdered = 'Cake Ordered';
+
+//to implement an action, an action creator needs to be created.
+// JS APP - Action - Reducer - State
+
+//Action Implemented
+const orderCake = () => {
+    return {
+        type: cakeOrdered,
+        quantity: 1
+    }
+}
+
+const initialState = {
+    numOfCakes: 10
+}
+
+// Reducers Update the current state of the application.
+// Reducer Implemented
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case cakeOrdered:
+            return { 
+                numOfCakes: state.numOfCakes - 1
+            }
+        default: 
+            return state;
+    }
+}
+
+// handling a stateobject change with more than 1 property
+const initialState2 = {
+    numOfCakes: 10,
+    anotherProperty: 0
+}
+
+//to handle this, it's good to create a copy of the state object
+const reducer2 = (state = initialState, action) => {
+    switch (action.type) {
+        case cakeOrdered:
+            return {
+                ...state, // this spread operator creates a copy of the state object
+                // it would just print out:
+                // numOfCakes: 10,
+                // anotherProperty: 0,
+                // then it updates the one you want to update  
+                numOfCakes: state.numOfCakes - 1
+            }
+        default: 
+            return state;
+    }
+}
+
+//creating the store
+// Store Responsibilities
+// Hold APP State
+// Allow access to state via getState()
+// allow state to be updated via dispatch(action)
+//Register listeners via subscribe(listener)
+// Handle unrregistering of listeners via the fxn returned by subscribe(listener)
+
+// Hold App State
+const store = createStore(reducer);
+
+// Allow access to state 
+console.log('Initial State', store.getState());
+
+// jump to step  for now 
+// Register Listeners via subscribe(listener)
+store.subscribe(() => console.log('update state', store.getState()))
+
+// Dispatch method accepts a function as a parameter, create the fxn there or 
+//use an already created one, there is a created one in this case
+store.dispatch(orderCake())
+//to cause more state changes:
+store.dispatch(orderCake())
+store.dispatch(orderCake())
+
+// for the unsubscribe method, just call the function that is returned from the subscribe method 
+const unsubscribe = store.subscribe(() => console.log('update state', store.getState()))
+
+unsubscribe();
+
